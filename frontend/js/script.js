a4af9de571647302ce54e60a05aa681a73d77b89
PedroFricensaft <pedrofricensaftdasilva@gmail.com>
2025-11-17 22:35:42 +0000

    NOVO

---
+++ frontend/js/script.js
@@ -0,0 +1,6 @@
+const addButton=document.querySelector(".add-task-btn");
+const sectionAddTask =document.querySelector(".newtask");
+
+addButton.addEventListener('click',()=>{
+    sectionAddTask.display
+})
